0a540c6d0b52c60a9f88354806deaad3a5c0a0ed
Yedelo <158670400+Yedelo@users.noreply.github.com>
2025-08-27 22:27:55 -0400

    Add true list difficulty button

--- src/TrueLevelListLayer.cpp
+++ src/TrueLevelListLayer.cpp
@@ -3,12 +3,24 @@
 using namespace geode::prelude;
 
 #include <Geode/modify/LevelListLayer.hpp>
-class $modify(LevelListLayer) {
+class $modify(TrueLevelListLayer, LevelListLayer) {
     $override bool init(GJLevelList* list) {
         if (!LevelListLayer::init(list)) return false;
         
-        log::info("Hello level list layer");
+        if (Mod::get()->getSettingValue<bool>("enabled")) {
+            CCMenu* rightSideMenu = static_cast<CCMenu*>(getChildByID("right-side-menu"));
+            CircleButtonSprite* sprite = CircleButtonSprite::createWithSprite("logo.png"_spr);
+            sprite->setScale(1 / 1.26);
+            CCMenuItemSpriteExtra* button = CCMenuItemSpriteExtra::create(sprite, this, menu_selector(TrueLevelListLayer::showTrueListDifficulty));
+            button->setID("true-list-difficulty-button");
+            rightSideMenu->addChild(button);
+            rightSideMenu->updateLayout();
+        }
 
         return true;
+    }
+
+    void showTrueListDifficulty(CCObject* sender) {
+        log::info("showTrueListDifficulty");
     }
 };
