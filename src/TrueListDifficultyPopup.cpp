0d205434bfc142d517e0c6d3e6d73c68a2f4453c
Yedelo <158670400+Yedelo@users.noreply.github.com>
2025-08-29 20:57:59 -0400

    Add included difficulties label and remove weight and height as constants

--- src/TrueListDifficultyPopup.cpp
+++ src/TrueListDifficultyPopup.cpp
@@ -4,12 +4,9 @@ using namespace geode::prelude;
 
 #include "TrueListDifficultyPopup.hpp"
 
-#define WIDTH 300
-#define HEIGHT 300
-
 TrueListDifficultyPopup* TrueListDifficultyPopup::create(LevelListLayer* layer) {
     TrueListDifficultyPopup* result = new TrueListDifficultyPopup();
-    if (result->initAnchored(WIDTH, HEIGHT, layer)) {
+    if (result->initAnchored(240, 240, layer)) {
         result->autorelease();
         return result;
     }
@@ -21,27 +18,31 @@ bool TrueListDifficultyPopup::setup(LevelListLayer* layer) {
     setTitle("True List Difficulty");
     setID("true-list-difficulty-popup");
 
-    CCMenu* trueDifficultyMenu = createAndAddMenu("true-difficulty-menu");
-    CCLabelBMFont* trueDifficultyLabel = CCLabelBMFont::create("True Difficulty", "bigFont.fnt");
-    trueDifficultyMenu->addChild(trueDifficultyLabel);
-    GJDifficultySprite* trueDifficultySprite = GJDifficultySprite::create(static_cast<int>(GJDifficulty::Harder), GJDifficultyName::Short);
-    trueDifficultyMenu->addChild(trueDifficultySprite);
+    CCMenu* generalMenu = createAndAddMenu("general-menu");
+    generalMenu->ignoreAnchorPointForPosition(true);
+    generalMenu->setScale(0.4);
+    generalMenu->setPosition({120, 144});
+    CCLabelBMFont* includedDifficultiesLabel = CCLabelBMFont::create("Included Difficulties", "bigFont.fnt");
+    includedDifficultiesLabel->setID("included-difficulties-label");
+    generalMenu->addChildAtPosition(includedDifficultiesLabel, Anchor::Top, {0, -20});
+
     CCMenu* meanMenu = createAndAddMenu("mean-menu");
     CCLabelBMFont* meanLabel = CCLabelBMFont::create("Mean", "bigFont.fnt");
     meanMenu->addChild(meanLabel);
     GJDifficultySprite* meanSprite = GJDifficultySprite::create(static_cast<int>(GJDifficulty::Easy), GJDifficultyName::Short);
     meanMenu->addChild(meanSprite);
+
     CCMenu* medianMenu = createAndAddMenu("median-menu");
     CCLabelBMFont* medianLabel = CCLabelBMFont::create("Median", "bigFont.fnt");
     medianMenu->addChild(medianLabel);
     GJDifficultySprite* medianSprite = GJDifficultySprite::create(static_cast<int>(GJDifficulty::Normal), GJDifficultyName::Short);
     medianMenu->addChild(medianSprite);
+
     CCMenu* modeMenu = createAndAddMenu("mode-menu");
     CCLabelBMFont* modeLabel = CCLabelBMFont::create("Mode", "bigFont.fnt");
     modeMenu->addChild(modeLabel);
     GJDifficultySprite* modeSprite = GJDifficultySprite::create(static_cast<int>(GJDifficulty::Hard), GJDifficultyName::Short);
     modeMenu->addChild(modeSprite);
-    
 
 
     return true;
